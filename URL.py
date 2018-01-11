48ada9d5c9f956ba915646b3a0fd7395fdfff416
kirillcheck <32092781+kirillcheck@users.noreply.github.com>
2018-01-11 11:35:50 +0300

    Update URL.py

--- URL.py
+++ URL.py
@@ -11,7 +11,7 @@ import pprint #красивый вид
 print('Введите id:')  
 id = input()    # на вход подаём цифру-id пользователя на выходе имеем некоторые основные данные
 hack = "https://api.vk.com/method/users.get?user_ids={id}&fields=education&v=5.69".format(id = id)
-f1 = urlopen(hacker_man)
+f1 = urlopen(hack)
 
 obj = json.loads(f1.read())
 print(hack)
